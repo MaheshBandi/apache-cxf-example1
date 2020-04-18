b40055d66a871de5070e2cbdfa98c6b318e50dbf
Mahesh Bandi <mbandi1985@gmail.com>
2020-04-18 11:34:15 +0530

    Added Logging Feature

---
+++ src/test/java/com/mahesh/ws/example/ApacheCxfExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.ws.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCxfExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
