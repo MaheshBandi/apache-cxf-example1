af38e5a0bfcdc35451213bec56d01e74682a8238
Mahesh Bandi <mbandi1985@gmail.com>
2020-04-18 11:30:40 +0530

    Added Logging Feature

--- src/main/java/com/mahesh/ws/example/HelloWS.java
+++ src/main/java/com/mahesh/ws/example/HelloWS.java
@@ -1,9 +1,12 @@
 package com.mahesh.ws.example;
 
+import org.apache.cxf.feature.Features;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
+@Features(features = "org.apache.cxf.feature.LoggingFeature")
 public class HelloWS {
     @WebMethod
     public String hello(){
